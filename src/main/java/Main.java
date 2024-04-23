16a2866a7a5cf7082e3d42d76b5d6d2864e67d73
Nick Zuzow <zuzownic@gmail.com>
2024-04-23 14:46:40 -0400

    stage 3: add 404 response if path is not /

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
@@ -18,11 +20,29 @@ public class Main {
       serverSocket = new ServerSocket(4221);
       serverSocket.setReuseAddress(true);
       clientSocket = serverSocket.accept(); // Wait for connection from client.
-      InputStream is = clientSocket.getInputStream();
       System.out.println("accepted new connection");
+      
+      // read the input
+      InputStream is = clientSocket.getInputStream();
+      BufferedReader brIn = new BufferedReader(new InputStreamReader(is));
+
+      // read the first line of the request and create a string array of the parts within
+      // the start line, delimited by spaces.
+      String[] sLineParts = brIn.readLine().split(" ");
+      String path = sLineParts[1];
+      
+      // create an output stream and print writer for the response
       OutputStream os = clientSocket.getOutputStream();
       PrintWriter writer = new PrintWriter(os, true);
-      writer.println("HTTP/1.1 200 OK\r\n\r\n");
+
+      // determine proper response based on path
+      String outLine = "";
+      if ("/".equals(path)) {
+        outLine = "HTTP/1.1 200 OK\r\n\r\n";
+      } else {
+        outLine = "HTTP/1.1 404 Not Found\r\n\r\n";
+      }
+      writer.println(outLine);
       clientSocket.close();
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
