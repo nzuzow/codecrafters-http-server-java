b7a5024c7980c35f01a116e67ab1fc4f4fdd1e01
Nick Zuzow <zuzownic@gmail.com>
2024-04-24 17:00:28 -0400

    stage 7: Add endpoint to get the contents of a file

--- src/main/java/ClientHandler.java
+++ src/main/java/ClientHandler.java
@@ -2,14 +2,18 @@ import java.net.Socket;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.util.Map;
 
 public class ClientHandler implements Runnable {
     private Socket clientSocket;
+    private Map<String, String> cfg;
 
-    public ClientHandler(Socket clientSocket) {
+    public ClientHandler(Socket clientSocket, Map<String, String> cfg) {
         this.clientSocket = clientSocket;
+        this.cfg = cfg;
     }
 
     @Override
@@ -37,22 +41,45 @@ public class ClientHandler implements Runnable {
                 }
             }
             
-            String path = req.getPath();
+            String path = (req.getPath() != null) ? req.getPath() : "";
 
             // determine proper response based on path
-            // ResponseItem resp;
             HttpResp resp;
-            if (path != null && path.startsWith("/echo")) {
-                String extraPath = (path.startsWith("/echo/")) ? path.substring(6) : path.substring(5);
+            if (path.startsWith("/files/")) {
+                try {
+                    String fileName = path.substring(7);
+                    String dir = this.cfg.get("--directory");
+                    BufferedReader file = new BufferedReader(new FileReader(dir + fileName));
+                    StringBuilder fileSB = new StringBuilder();
+                    String line;
+                    while (file.ready() && (line = file.readLine()) != null) {
+                        fileSB.append(line);
+                    }
+                    file.close();
+                    String body = fileSB.toString();
+
+                    resp = new HttpRespBuilder()
+                            .version("HTTP/1.1")
+                            .statusCode(200)
+                            .statusText("OK")
+                            .addHeader("Content-Type", "application/octet-stream")
+                            .addHeader("Content-Length", String.valueOf(body.length()))
+                            .body(body)
+                            .build();
+                } catch(Exception e) {
+                    resp = buildErrorResp();
+                }
+            } else if (path.startsWith("/echo")) {
+                String body = (path.startsWith("/echo/")) ? path.substring(6) : path.substring(5);
                 resp = new HttpRespBuilder()
                         .version("HTTP/1.1")
                         .statusCode(200)
                         .statusText("OK")
                         .addHeader("Content-Type", "text/plain")
-                        .addHeader("Content-Length", String.valueOf(extraPath.length()))
-                        .body(extraPath)
+                        .addHeader("Content-Length", String.valueOf(body.length()))
+                        .body(body)
                         .build();
-            } else if (path != null && path.startsWith("/user-agent")) {
+            } else if (path.startsWith("/user-agent")) {
                 String body = req.getHeader("User-Agent");
                 resp = new HttpRespBuilder()
                         .version("HTTP/1.1")
@@ -71,13 +98,7 @@ public class ClientHandler implements Runnable {
                         .addHeader("Content-Length", "0")
                         .build();
             } else {
-                resp = new HttpRespBuilder()
-                        .version("HTTP/1.1")
-                        .statusCode(404)
-                        .statusText("Not Found")
-                        .addHeader("Content-Type", "text/plain")
-                        .addHeader("Content-Length", "0")
-                        .build();
+                resp = buildErrorResp();
             }
 
             // create an output stream and print writer for the response
@@ -86,9 +107,20 @@ public class ClientHandler implements Runnable {
             writer.println(resp.asString());
             writer.close();
             in.close();
+            os.close();
             this.clientSocket.close();
         } catch (Exception e) {
             System.out.println("ClientHandler Exception: " + e.getMessage());
         }
+    }
+
+    private HttpResp buildErrorResp() {
+        return new HttpRespBuilder()
+                .version("HTTP/1.1")
+                .statusCode(404)
+                .statusText("Not Found")
+                .addHeader("Content-Type", "text/plain")
+                .addHeader("Content-Length", "0")
+                .build();
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,16 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.HashMap;
 
 public class Main {
   public static void main(String[] args) {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
+    
+    // Get a config map from the arguments passed in from the command line
+    Map<String, String> cfg = buildConfig(args);
 
     // Uncomment this block to pass the first stage
     ServerSocket serverSocket = null;
@@ -19,11 +24,24 @@ public class Main {
         clientSocket = serverSocket.accept(); // Wait for connection from client.
         System.out.println("accepted new connection");
 
-        Thread clientThread = new Thread(new ClientHandler(clientSocket));
+        Thread clientThread = new Thread(new ClientHandler(clientSocket, cfg));
         clientThread.start();
       }
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     }
+  }
+
+  private static Map<String, String> buildConfig(String[] args) {
+    // Loop through the arguments passed in and add them to a config map.
+    // Currently assuming arguments will be defined on the command line as <key> <value>, so
+    // increment through the argument list by 2.
+    Map<String, String> output = new HashMap<String, String>();
+    int i = 0;
+    while (i < args.length - 1) {
+      output.put(args[i], args[i + 1]);
+      i += 2;
+    }
+    return output;
   }
 }
